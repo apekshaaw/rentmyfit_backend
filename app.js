0830c5686a4db1d155b611dd21f38463b97cf79d
apekshaaw <apeksha.wagle2017@gmail.com>
2025-07-30 00:28:13 +0545

    app

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// app.js
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
@@ -15,7 +14,8 @@ app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true,
 }));
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 
 // Routes
 app.use('/api/auth', authRoutes);
