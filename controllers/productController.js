c38bba8cc0cc17d92e57d1a14bf48b2ce5bb7dc2
apekshaaw <apeksha.wagle2017@gmail.com>
2025-07-31 16:11:33 +0545

    feat: add product update and delete API handlers

--- controllers/productController.js
+++ controllers/productController.js
@@ -30,10 +30,14 @@ export const getProductById = async (req, res) => {
 
 export const updateProduct = async (req, res) => {
   try {
-    const updated = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.json(updated);
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true } 
+    );
+    res.status(200).json(updatedProduct); 
   } catch (err) {
-    res.status(400).json({ message: err.message });
+    res.status(500).json({ message: err.message });
   }
 };
 
